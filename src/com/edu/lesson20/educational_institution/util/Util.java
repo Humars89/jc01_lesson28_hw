1bc5514402feffe7f1d541f4355158f1579e5995
Humars89 <sgumar89@gmail.com>
2025-06-26 19:27:15 +0300

    Delete src directory

--- src/com/edu/lesson20/educational_institution/util/Util.java
+++
@@ -1,7 +0,0 @@
-package com.edu.lesson20.educational_institution.util;
-
-public class Util {
-    public static boolean isNotEmpty(String str) {
-        return str != null && !str.trim().isEmpty();
-    }
-} 
